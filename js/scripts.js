4afac70f7c85b503868a115fae82cd12a912d3e2
Matt Miller <matt.f.miller@live.com>
2018-04-06 11:24:40 -0700

    add logic to add multiple pizza objects to order object.

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,12 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.total = function() {
-  if (this.size === '18"') {
+  if (this.size === "18") {
     return "$" + (18 + this.toppings.length);
   }
-  else if (this.size === '15"') {
+  else if (this.size === "15") {
     return "$" + (15 + this.toppings.length);
-  } else if (this.size === '12"') {
+  } else if (this.size === "12") {
     return "$" + (12 + this.toppings.length);
   }
 }
@@ -23,17 +23,57 @@ Pizza.prototype.total = function() {
 
 // USER LOGIC
 $(function() {
+  $("#add-pizza").click(function() {
+    $("#new-pizzas").append('<div class="new-pizza">' +
+                              '<div class="form-group size-form">' +
+                                '<label for="order-size">Size</label>' +
+                                '<select class="form-control order-size">' +
+                                  '<option value="18">18"</option>' +
+                                  '<option value="15">15"</option>' +
+                                  '<option value="12">12"</option>' +
+                                '</select>' +
+                              '</div>' +
+                              '<div class="form-group">' +
+                                '<label for="toppings">Toppings</label><br>' +
+                                '<input type="checkbox" name="topping-option" value="pepperoni">Pepperoni<br>' +
+                                '<input type="checkbox" name="topping-option" value="anchovy">Anchovy<br>' +
+                                '<input type="checkbox" name="topping-option" value="artichoke">Artichoke<br>' +
+                                '<input type="checkbox" name="topping-option" value="mushrooms">Mushrooms<br>' +
+                                '<input type="checkbox" name="topping-option" value="sausage">Sausage<br>' +
+                                '<input type="checkbox" name="topping-option" value="carmelized-onions">Carmelized Onions<br>' +
+                              '</div>' +
+                            '</div>')
+  });
+
   $("#order-form").submit(function(event){
     var orderName = $("#order-name").val();
-    var orderSize = $("#order-size").val();
-    var pizza = new Pizza(orderSize);
-    $("input:checkbox[name=topping-option]:checked").each(function(){
-      var orderTopping = $(this).val();
-      pizza.toppings.push(orderTopping);
-    });
-    var orderTotal = pizza.total();
-    $("#order-total").text(orderTotal);
-    $(".no-display").fadeIn();
+    var order = new Order(orderName);
+    $(".new-pizza").each(function(){
+      var orderSize = $(this).find("select.order-size").val();
+      var pizza = new Pizza(orderSize);
+      $(this).find("input:checkbox[name=topping-option]:checked").each(function(){
+        var orderTopping = $(this).val();
+        pizza.toppings.push(orderTopping);
+      });
+      order.pizzas.push(pizza);
+    })
+
+    // console.log(pizza);
+    console.log(order);
+
+
+    // var orderTotal = pizza.total();
+    // $("#order-total").text(orderTotal);
+    // $(".no-display").fadeIn();
     event.preventDefault();
   });
 });
+
+
+// var orderSize = $(".order-size").val();
+// var pizza = new Pizza(orderSize);
+// $("input:checkbox[name=topping-option]:checked").each(function(){
+//   var orderTopping = $(this).val();
+//   pizza.toppings.push(orderTopping);
+// });
+// order.pizzas.push(pizza);
